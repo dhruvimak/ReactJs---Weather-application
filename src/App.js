533b5101cef389168771da714b606994d148b4ea
dhruvimak <dhruvipmakavana@yahoo.com>
2018-08-29 23:58:01 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ getWeather = async(e) => {
   e.preventDefault();
   const city=e.target.elements.city.value;
   const country=e.target.elements.country.value;
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}`);
+  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}`);
   const data = await api_call.json();
   console.log(data);
 
