64fcb70eed836002f6e19e6c8f97a28a922075bf
dhruvimak <dhruvipmakavana@yahoo.com>
2018-08-30 16:08:28 +0530

    error handled -2.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import Titles from './Components/Titles';
 import Form from './Components/Form';
 import Weather from './Components/Weather';
@@ -24,25 +26,29 @@ class App extends Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
-    if(data.cod==404){
-      this.setState({
-        temp: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "city not found"
-      })
-    }
-    else if(city && country) {
-      this.setState({
-        temp: data.list[0].main.temp,
-        city: data.city.name,
-        country: data.city.country,
-        humidity: data.list[0].main.humidity,
-        description: data.list[0].weather[0].description,
-        error: ""
-      });
+    if (city && country) {
+
+      if (data.cod == 404) {
+        this.setState({
+          temp: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "city not found"
+        })
+      } else {
+        this.setState({
+          temp: data.list[0].main.temp,
+          city: data.city.name,
+          country: data.city.country,
+          humidity: data.list[0].main.humidity,
+          description: data.list[0].weather[0].description,
+          error: ""
+        });
+      }
+
+
     } else {
       this.setState({
         temp: undefined,
@@ -57,29 +63,35 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="wrapper">
-        <div className="main">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-5 title-container">
-                <Titles />
-              </div>
-              <div className="col-sm-7 form-container">
-                <Form getWeather={this.getWeather} />
-                <Weather
-                  temp={this.state.temp}
-                  city={this.state.city}
-                  country={this.state.country}
-                  humidity={this.state.humidity}
-                  description={this.state.description}
-                  error={this.state.error}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    return ( < div className = "wrapper" >
+      < div className = "main" >
+      < div className = "container" >
+      < div className = "row" >
+      < div className = "col-sm-5 title-container" >
+      < Titles / >
+      </div> < div className = "col-sm-7 form-container" >
+      < Form getWeather = {
+        this.getWeather
+      }
+      /> <      Weather temp = {
+        this.state.temp
+      }
+      city = {
+        this.state.city
+      }
+      country = {
+        this.state.country
+      }
+      humidity = {
+        this.state.humidity
+      }
+      description = {
+        this.state.description
+      }
+      error = {
+        this.state.error
+      }
+      /> </div> </div> </div> </div> </div>
     );
   }
 }
